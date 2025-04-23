fb1608d1fbb7a171ad48ca9ba63bea74ea894b14
TAURUS-ESSEN <reitarovea@gmail.com>
2025-04-23 16:14:39 +0200

    ubung 7 shon fertig

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 'use strict';
 
 let date = new Date();
-let geburtsJahr = 0;
 let heutigeJahr  = date.getFullYear();
-console.log(heutigeJahr)
+let alter = 0;
 
+// Übung 6
+let geburtsJahr = 0;
 const u6Form = document.getElementById('ubung6');
 const u6result = document.getElementById('u6result');
-const u6input = ubung6.querySelector("input");
+const u6input = u6Form.querySelector("input");
 const u6button = ubung6.querySelector("button");
 u6button.addEventListener("click", ()=> {
-    let alter = heutigeJahr - u6input.value;
-    console.log(u6input.value)
+    alter = heutigeJahr - u6input.value;
     u6result.textContent = `Im Jahr ${heutigeJahr} bin ich ${alter-1} oder ${alter} Jahre alt.`;
+    ubung7()
 })
 
-// console.log('Год:', d.getFullYear());
-// console.log('Месяц:', d.getMonth());        // 0-11
-// console.log('День месяца:', d.getDate());   // 1-31
-// console.log('День недели:', d.getDay());    // 0-6
-// console.log('Часы:', d.getHours());
-// console.log('Минуты:', d.getMinutes());
-// console.log('Секунды:', d.getSeconds());
-// console.log('Миллисекунды с 1970:', d.getTime());
-// for (let key of ['getFullYear', 'getMonth', 'getDate', 'getDay', 'getHours', 'getMinutes', 'getSeconds', 'getMilliseconds', 'getTime']) {
-//     console.log(key + ':', d[key]());
-// }
+// Übung 7
+    const u7result = document.getElementById('u7result');
+    const product = [
+        { productName: "Beutel Tee ", protag: 2 }, 
+        { productName: "Spaghetti ", protag: 0.5 }
+    ]
+    
+
+    function ubung7() {
+        let bisHundred  = 0;
+        let anzahlProducts = [];
+        bisHundred  = (100 -Number(alter))*364;
+     
+        u7result.innerHTML = `Dein Alter ist ${alter}, ${bisHundred} bleibd dir bis zum 100 (eigentlich nicht viel, das Leben is leider kurz)und du brauchst mindestens: <br>`;
+        for (let i=0; i<product.length; i++) {
+            anzahlProducts = bisHundred * product[i].protag; 
+            u7result.innerHTML += `<br> ${product[i].productName} :  ${anzahlProducts} (${product[i].protag} pro Tag)`
+        }
+        
+    } 
